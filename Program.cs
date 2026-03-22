07d29cc55864cea6fd336db5030b6371c7c6258f
actividad-c- <luuisahurtado2005@gmail.com>
2026-03-21 22:01:06 -0500

    feat: add main menu navigation

--- Program.cs
+++ Program.cs
@@ -2,10 +2,61 @@
 
 class Program
 {
-    static void Main()
+    static void Main()    
     {
         ShowMainMenu();
-    }
+       }
 
-    static void ShowMainMenu() { }
+    static void  
+    ShowMainMenu()
+    {
+    int option = 0;
+
+    do
+    {
+        Console.Clear();
+        Console.WriteLine("=== SISTEMA DE BIBLIOTECA ===");
+        Console.WriteLine("1. Libros");
+        Console.WriteLine("2. Usuarios");
+        Console.WriteLine("3. Préstamos");
+        Console.WriteLine("4. Búsquedas y reportes");
+        Console.WriteLine("5. Guardar / Cargar datos");
+        Console.WriteLine("6. Salir");
+        Console.Write("Seleccione una opción: ");
+
+        int.TryParse(Console.ReadLine(), out option);
+
+        switch (option)
+        {
+            case 1:
+                ShowBooksMenu();
+                break;
+
+            case 2:
+                ShowUsersMenu();
+                break;
+
+            case 3:
+                ShowLoansMenu();
+                break;
+
+            case 4:
+                ShowSearchReportsMenu();
+                break;
+
+            case 5:
+                ShowPersistenceMenu();
+                break;
+
+            case 6:
+                ConfirmExitAndSave();
+                break;
+
+            default:
+                Console.WriteLine("Opción inválida");
+                Console.ReadKey();
+                break;
+        }
+
+    } while (option != 6);
 }
